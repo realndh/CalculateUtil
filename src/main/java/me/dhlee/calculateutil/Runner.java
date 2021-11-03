0cc907b67860f79c8ef6540c873674e5f10e4f7f
realndh <realndh@naver.com>
2021-11-03 23:11:56 +0900

    수정

--- src/main/java/me/dhlee/calculateutil/Runner.java
+++ src/main/java/me/dhlee/calculateutil/Runner.java
@@ -6,25 +6,38 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+import java.util.stream.DoubleStream;
 
 @Component
 @Slf4j
 public class Runner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        String[][] strArr = new String[100][1000];
+        String[][] strArr = new String[10][1];
 
         for (int i = 0; i < strArr.length; i++) {
-            for (int j = 0; j < strArr[i].length; j++) {
-                strArr[i][j] = (i + 1) * (j + 1) + "";
+            StringBuilder stringBuilder = new StringBuilder();
+            String separator = "";
+            for (int j = 0; j < 10; j++) {
+                stringBuilder.append(separator);
+                stringBuilder.append(i * 10 + j + "");
+                separator = ",";
             }
+            strArr[i][0] = stringBuilder.toString();
         }
 
-        Double min = Arrays.stream(strArr).flatMapToDouble(x -> Arrays.stream(x).mapToDouble(Double::parseDouble)).min().getAsDouble();
-        Double max = Arrays.stream(strArr).flatMapToDouble(x -> Arrays.stream(x).mapToDouble(Double::parseDouble)).max().getAsDouble();
-        Double sum = Arrays.stream(strArr).flatMapToDouble(x -> Arrays.stream(x).mapToDouble(Double::parseDouble)).sum();
-        Double avg = Arrays.stream(strArr).flatMapToDouble(x -> Arrays.stream(x).mapToDouble(Double::parseDouble)).average().getAsDouble();
+        DoubleStream doubleStream = Arrays.stream(strArr).flatMapToDouble(x -> Arrays.stream(x).flatMapToDouble(y -> Arrays.stream(y.split(",")).mapToDouble(Double::parseDouble)));
+//        doubleStream.forEach(System.out::println);
+        System.out.println(doubleStream.max().getAsDouble());
 
-        log.info("min = {}, max = {}, sum = {}, avg = {}", min, max, sum, avg);
+//        System.out.println(result);
+//        Double min = Arrays.stream(strArr).flatMapToDouble(x -> Arrays.stream(x).mapToDouble(Double::parseDouble)).min().getAsDouble();
+//        Double max = Arrays.stream(strArr).flatMapToDouble(x -> Arrays.stream(x).mapToDouble(Double::parseDouble)).max().getAsDouble();
+//        Double sum = Arrays.stream(strArr).flatMapToDouble(x -> Arrays.stream(x).mapToDouble(Double::parseDouble)).sum();
+//        Double avg = Arrays.stream(strArr).flatMapToDouble(x -> Arrays.stream(x).mapToDouble(Double::parseDouble)).average().getAsDouble();
+//
+//        log.info("min = {}, max = {}, sum = {}, avg = {}", min, max, sum, avg);
+
+
     }
 }
